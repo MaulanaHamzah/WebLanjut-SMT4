fda0714c5a31c13dbc16f30678eea5f4c9fa04d2
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2025-03-22 13:33:53 +0700

    Update PWLPOS versi baru

---
+++ PWLPOS.v2/app/Http/Controllers/BarangController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BarangModel;
+use App\Models\KategoriModel;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
+
+class BarangController extends Controller
+{
+    public function index()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Daftar Barang',
+            'list' => ['Home', 'Barang']
+        ];
+
+        $page = (object)[
+            'title' => 'Daftar barang yang terdaftar dalam sistem'
+        ];
+
+        $activeMenu = 'barang';
+
+        $kategori = KategoriModel::all();
+
+        return view('barang.index', compact('breadcrumb', 'page', 'kategori', 'activeMenu'));
+    }
+
+    public function list(Request $request)
+    {
+        $barangs = BarangModel::with('kategori')
+            ->select('barang_id', 'kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual');
+
+        if ($request->kategori_id) {
+            $barangs->where('kategori_id', $request->kategori_id);
+        }
+
+        return DataTables::of($barangs)
+            ->addIndexColumn()
+            ->addColumn('kategori_nama', function ($barang) {
+                return $barang->kategori ? $barang->kategori->kategori_nama : '-';
+            })
+            ->addColumn('aksi', function ($barang) {
+                $btn = '<a href="' . url('/barang/' . $barang->barang_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/barang/' . $barang->barang_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/barang/' . $barang->barang_id) . '">' .
+                    csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
+                return $btn;
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+
+    public function create()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Tambah Barang',
+            'list' => ['Home', 'Barang', 'Tambah']
+        ];
+
+        $page = (object)[
+            'title' => 'Tambah barang baru'
+        ];
+
+        $kategori = KategoriModel::all();
+        $activeMenu = 'barang';
+
+        return view('barang.create', compact('breadcrumb', 'page', 'kategori', 'activeMenu'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'barang_kode' => 'required|string|min:6|max:10|unique:m_barang,barang_kode',
+            'barang_nama' => 'required|string|max:100',
+            'harga_beli' => 'required|integer|min:0',
+            'harga_jual' => 'required|integer|min:0',
+            'kategori_id' => 'required|exists:m_kategori,kategori_id'
+        ]);
+
+        BarangModel::create($request->all());
+
+        return redirect('/barang')->with('success', 'Data barang berhasil disimpan');
+    }
+
+    public function show($id)
+    {
+        $barang = BarangModel::with('kategori')->findOrFail($id);
+
+        $breadcrumb = (object)[
+            'title' => 'Detail Barang',
+            'list' => ['Home', 'Barang', 'Detail']
+        ];
+
+        $page = (object)[
+            'title' => 'Detail barang'
+        ];
+
+        $activeMenu = 'barang';
+
+        return view('barang.show', compact('breadcrumb', 'page', 'barang', 'activeMenu'));
+    }
+
+    public function edit($id)
+    {
+        $barang = BarangModel::findOrFail($id);
+        $kategori = KategoriModel::all();
+
+        $breadcrumb = (object)[
+            'title' => 'Edit Barang',
+            'list' => ['Home', 'Barang', 'Edit']
+        ];
+
+        $page = (object)[
+            'title' => 'Edit barang'
+        ];
+
+        $activeMenu = 'barang';
+
+        return view('barang.edit', compact('breadcrumb', 'page', 'barang', 'kategori', 'activeMenu'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $barang = BarangModel::findOrFail($id);
+
+        $request->validate([
+            'barang_kode' => 'required|string|min:6|max:10|unique:m_barang,barang_kode,' . $id . ',barang_id',
+            'barang_nama' => 'required|string|max:100',
+            'harga_beli' => 'required|integer|min:0',
+            'harga_jual' => 'required|integer|min:0',
+            'kategori_id' => 'required|exists:m_kategori,kategori_id'
+        ]);
+
+        $barang->update($request->all());
+
+        return redirect('/barang')->with('success', 'Data barang berhasil diperbarui');
+    }
+
+    public function destroy($id)
+    {
+        $barang = BarangModel::find($id);
+        if (!$barang) {
+            return redirect('/barang')->with('error', 'Data barang tidak ditemukan');
+        }
+
+        try {
+            $barang->delete();
+            return redirect('/barang')->with('success', 'Data barang berhasil dihapus');
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect('/barang')->with('error', 'Data barang gagal dihapus karena masih terkait dengan data lain');
+        }
+    }
+}
---
+++ PWLPOS.v2/routes/web.php
@@ -0,0 +1,94 @@
+<?php
+
+use App\Http\Controllers\BarangController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\StokController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);          //Menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);      //Menampilkan data user dalam bentuk json
+    Route::get('/create', [UserController::class, 'create']);   //Menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);         //Menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);       //Menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);  //Menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);     //Menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); //Menghapus data user
+});
+
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/', [LevelController::class, 'index']);
+    Route::post('/list', [LevelController::class, 'list']);
+    Route::get('/create', [LevelController::class, 'create']);
+    Route::post('/', [LevelController::class, 'store']);
+    Route::get('/{id}', [LevelController::class, 'show']);
+    Route::get('/{id}/edit', [LevelController::class, 'edit']);
+    Route::put('/{id}', [LevelController::class, 'update']);
+    Route::delete('/{id}', [LevelController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'kategori'], function () {
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::post('/list', [KategoriController::class, 'list']);
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{id}', [KategoriController::class, 'show']);
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+    Route::put('/{id}', [KategoriController::class, 'update']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'barang'], function () {
+    Route::get('/', [BarangController::class, 'index']);
+    Route::post('/list', [BarangController::class, 'list']);
+    Route::get('/create', [BarangController::class, 'create']);
+    Route::post('/', [BarangController::class, 'store']);
+    Route::get('/{id}', [BarangController::class, 'show']);
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);
+    Route::put('/{id}', [BarangController::class, 'update']);
+    Route::delete('/{id}', [BarangController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'supplier'], function () {
+    Route::get('/', [SupplierController::class, 'index']);
+    Route::post('/list', [SupplierController::class, 'list']);
+    Route::get('/create', [SupplierController::class, 'create']);
+    Route::post('/', [SupplierController::class, 'store']);
+    Route::get('/{id}', [SupplierController::class, 'show']);
+    Route::get('/{id}/edit', [SupplierController::class, 'edit']);
+    Route::put('/{id}', [SupplierController::class, 'update']);    
+    Route::delete('/{id}', [SupplierController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'stok'], function () {
+    Route::get('/', [StokController::class, 'index']);
+    Route::post('/list', [StokController::class, 'list']);
+    Route::get('/create', [StokController::class, 'create']);
+    Route::post('/', [StokController::class, 'store']);
+    Route::get('/{id}', [StokController::class, 'show']);
+    Route::get('/{id}/edit', [StokController::class, 'edit']);
+    Route::put('/{id}', [StokController::class, 'update']);
+    Route::delete('/{id}', [StokController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'penjualan'], function () {
+    Route::get('/', [PenjualanController::class, 'index']);
+    Route::post('/list', [PenjualanController::class, 'list']);
+    Route::get('/create', [PenjualanController::class, 'create']);
+    Route::post('/', [PenjualanController::class, 'store']);
+    Route::get('/{id}', [PenjualanController::class, 'show']);
+    Route::get('/{id}/edit', [PenjualanController::class, 'edit']);
+    Route::put('/{id}', [PenjualanController::class, 'update']);
+    Route::delete('/{id}', [PenjualanController::class, 'destroy']);
+});
